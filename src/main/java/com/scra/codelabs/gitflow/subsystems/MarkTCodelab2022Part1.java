c4eef135fcc6896a25e1df283f30c8c69609d0a5
Mark Tomczak <fixermark@gmail.com>
2022-07-22 18:03:19 -0400

    Chanted message to ready

--- src/main/java/com/scra/codelabs/gitflow/subsystems/MarkTCodelab2022Part1.java
+++ src/main/java/com/scra/codelabs/gitflow/subsystems/MarkTCodelab2022Part1.java
@@ -4,6 +4,6 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class MarkTCodelab2022Part1 extends SubsystemBase{
     public MarkTCodelab2022Part1() {
-        System.out.println("MarkT subsystem operational.");
+        System.out.println("MarkT subsystem ready.");
     }
 }
